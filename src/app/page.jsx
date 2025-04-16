f7f1f7fdcef5e75997f4c82bef4fff5080395b5b
User244999 <147624050+notcore@users.noreply.github.com>
2025-04-17 00:18:46 +0800

    fix bug

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,38 @@
 "use client";
 import Image from "next/image";
-import { useEffect,useRef } from "react";
+// import { useEffect,useRef } from "react";
 // import * as motion from "motion/react-client"
 import { Portofolio } from "@/app/components/portofolio"
 import { Logo } from "@/app/components/logo"
 import { Read } from "@/app/components/read" 
 import { Faq } from "@/app/components/faq" 
 import { Footer } from "@/app/components/footer";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="">
-      <Portofolio />
-      <Logo />
-      <Read />
-      <Faq />
-      <Footer />
-    </div>
+    <>
+    {loading ? (
+      <div className="fixed inset-0 bg-white flex justify-center items-center z-50">
+        <div className="animate-spin border-t-4 border-blue-500 rounded-full w-12 h-12 border-4"></div>
+      </div>
+    ) : (
+      <>
+        <Portofolio />
+        <Logo />
+        <Read />
+        <Faq />
+        <Footer />
+      </>
+    )}
+  </>
   )
 }
